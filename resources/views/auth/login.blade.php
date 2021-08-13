b590c1209a9bd6e22c1bf8622fb1c50b08a26bd4
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2021-08-13 20:37:57 +0700

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -92,9 +92,10 @@
                     <div class="card">
                         <div class="content">
                             <div class="heading text-center">
-                                <p class="wow fadeInUp"><span class="purple"><i class="fas fa-paw"></i> </span><span
-                                        class="orange"><i class="fas fa-paw"></i> </span><span class="purple"><i
-                                            class="fas fa-paw"></i> </span></p>
+                                <p class="wow fadeInUp"><span class="purple"><i class="fas fa-paw"></i> </span>
+                                            <span class="orange"><i class="fas fa-paw"></i> </span>
+                                            <span class="purple"><i class="fas fa-paw"></i> </span>
+                                </p>
                                 <h3> <span class="wow pulse" data-wow-delay="1s"> Login
                                     </span></h3>
                             </div>
@@ -114,20 +115,43 @@
                                         </div>
                                         <div class="col-lg-12 col-md-12 col-sm-12">
                                                 <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                                     <label class="form-check-label" for="exampleCheck1">&nbsp;Remember Me</label>
                                                 </div>
-                                            </div>
+                                        </div>
+
                                         <div class="link text-center">
                                             <a href="{{ 'register' }}" class="d-inline-block" title="Register">Register</a> ||
                                             <a href="{{'password/reset'}}" class="d-inline-block"
                                                 title="Forgot Password">Forgot-Password</a>
                                         </div>
-                                        
                                         <div class="col-lg-12 col-md-12 col-sm-12">
                                             <div class="form-group text-center">
                                                 <button type="submit" class="btn form-control">Login</button>
                                             </div>
+                                        </div>
+                                        <div class="heading text-center">
+                                                <br>
+                                            <p class="wow fadeInUp">
+                                                <span class="wow pulse" data-wow-delay="1s"> Or Sign Up Using </span>
+                                            </p>
+                                            <center class="justify-content-xl-between" style="margin-top:10px;">
+                                                <a href="">  
+                                                    <span class="fa-stack fa-lg wow fadeInUp">
+                                                        <i class="fa fa-circle fa-stack-2x " style="color:#4267B2;"></i>
+                                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                                    </span>  
+                                                </a>
+                                                <a href="">  
+                                                    <span class="fa-stack fa-lg wow fadeInUp">
+                                                        <i class="fa fa-circle fa-stack-2x" style="color:#db4a39;"></i>
+                                                        <i class="fab fa-google-plus fa-stack-1x fa-inverse"></i>
+                                                    </span>
+                                                </a>
+                                                <a href="">  
+                                                    <img class="wow fadeInUp" src="peddyhub/images/home_5/icon-line.png" alt="" width="35px">
+                                                </a>
+                                            </center>
                                         </div>
                                     </div>
                                 </form>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pawrex Template | Error</title>
+    <link rel="stylesheet" href="css/imagehover.css">
+    <link href="{{ asset('peddyhub/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"> 
+    <link href="{{ asset('peddyhub/css/style.css') }}" rel="stylesheet"  type="text/css">
+    <link href="{{ asset('peddyhub/css/responsive.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('peddyhub/css/color-switcher-design.css') }}" rel="stylesheet">
+    <link id="theme-color-file" href="{{ asset('peddyhub/css/color-themes/default-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('peddyhub/images/home_5/favicon.png') }}" type="image/x-icon" rel="shortcut icon">
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+
+
+<body>
+    <div class="main-wrapper pet error">
+
+        <div id="preloading"></div>
+        <section class="featured">
+            <div class="header">
+                
+            </div>
+            <div class="crumb">
+                <div class="container">
+                    <h1>
+                        PeddyHub <span class="wow pulse" data-wow-delay="1s"> Error </span>
+                    </h1>
+                    <div class="bg_tran">
+                        PeddyHub Error
+                    </div>
+                    <p>
+                        <a href="{{ url('/') }}" title="Home">HOME</a>
+                        || <span> ERROR-404 </span>
+                    </p>
+                </div>
+            </div>
+
+        </section>
+        <section class="error">
+            <div class="container">
+                <div class="content text-center">
+                    <div class="an_image an_image_hz_ps_left an_image_vt_ps_middle an_image_anime_delay_300 an_image_anime_duration_1500 an_image_anime_style_ease_out_sine"
+                        data-speed="1.2">
+                        <div class="an_image_image" data-speed="1.2">
+                            <div class="pt_image">
+                                <span>
+                                    <img src="peddyhub/images/home_5/frisbee.png" title="Frisbee" alt="Image of Frisbee"
+                                        class="frisbee">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 class="yellow">404</h2>
+                    <h4>PAGE NOT <span> FOUND </span></h4>
+                    <h5 class="mb-5">Oops, the page you are looking for does not exit.</h5>
+                    <a href="{{ url('/') }}" class="btn main"><i class="fas fa-paw mr-2"></i> Back to Home</a>
+                    <div class="an_image an_image_hz_ps_right an_image_vt_ps_middle an_image_anime_delay_300 an_image_anime_duration_1500 an_image_anime_style_ease_out_sine"
+                        data-speed="1.2">
+                        <div class="an_image_image" data-speed="1.2">
+                            <div class="pt_image">
+                                <span>
+                                    <img src="peddyhub/images/home_5/ball.png" title="Pet" alt="Image of Pet" class="ball">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<br><br><br><br><br>
+            </div>
+        </section>
+
+
+        
+
+        <div class="pet">
+            <a id="back2Top" title="Back to top" href="#" class="hvr-radial-in"><i class="fas fa-paw"></i></a>
+        </div>
+
+        
+        
+        <div class="search-popup">
+            <button class="close-search style-two"><img src="peddyhub/images/home_5/cross-out.png" alt=""></button>
+            <button class="close-search"><img src="peddyhub/images/home_5/cross-out.png" alt=""></button>
+            <form method="post" action="blog.html">
+                <div class="form-group">
+                    <input type="search" name="search-field" value="" placeholder="Search Here" required>
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+    <script src="{{ asset('peddyhub/js/jquery.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/mousewheel.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/wow.min.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/vivus.min.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/script.js') }}"></script>
+    <script src="{{ asset('peddyhub/js/color-settings.js') }}"></script>  
+</body>
+
+</html>
