4dd6d4c29001c39c349f2325c5ea40a65f4630d8
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2021-08-18 15:59:10 +0700

    up

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('provider_id');
-            $table->string('provider');
-            $table->text('avatar');
+            $table->string('provider_id')->nullable();
+            $table->string('provider')->nullable();
+            $table->text('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
