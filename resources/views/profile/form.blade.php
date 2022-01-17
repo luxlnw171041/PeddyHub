a23112c9ed167491d7dabdf1bfdb70c59c19d915
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-01-17 16:23:56 +0700

    up

--- resources/views/profile/form.blade.php
+++ resources/views/profile/form.blade.php
@@ -116,6 +116,50 @@
                                 </div>
                                 
                             </div>
+                            <div class="heading">
+                                <p class="wow fadeInUp"><span class="purple"><i class="fas fa-paw"></i>
+                                    </span><span class="orange"><i class="fas fa-paw"></i> </span><span
+                                        class="purple"><i class="fas fa-paw"></i> </span></p>
+                                <h3>ภาษา <span class="wow pulse" data-wow-delay="1s"></span></h3>
+                            </div>
+                            <div class="row" style="margin-top:-20px;">
+                                <div class="col-md-4 col-lg-2 col-4">
+                                    <img class="btn" id="img_flag_en" style="filter: grayscale(100%);"  width="85" src="{{ url('/peddyhub/images/national-flag/flex-en.png') }}" onclick="change_language('en' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-md-4 col-lg-2 col-4" style="top:2px">
+                                    <img class="btn" id="img_flag_zh_TW" style="filter: grayscale(100%);"  width="85" src="{{ url('/peddyhub/images/national-flag/flex-zh-TW.png') }}" onclick="change_language('zh-TW' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2" style="top:4px">
+                                    <img class="btn" id="img_flag_in" style="filter: grayscale(100%); " width="82" src="{{ url('/peddyhub/images/national-flag/flex-in.png') }}" onclick="change_language('hi' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2" style="top:5px;right:-2px;">
+                                    <img class="btn" id="img_flag_ae" style="filter: grayscale(100%);"  width="79" src="{{ url('/peddyhub/images/national-flag/flex-ar.png') }}" onclick="change_language('ar' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2" style="top:5px;right:-2px;">
+                                    <img class="btn" id="img_flag_ru" style="filter: grayscale(100%); " width="79"  src="{{ url('/peddyhub/images/national-flag/flex-ru.png') }}" onclick="change_language('ru' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-md-4 col-lg-2 col-4" style="top:2px">
+                                    <img class="btn" id="img_flag_es" style="filter: grayscale(100%); " width="85"  src="{{ url('/peddyhub/images/national-flag/flex-es.png') }}" onclick="change_language('es' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-md-4 col-lg-2 col-4" style="top:2px">
+                                    <img class="btn" id="img_flag_de" style="filter: grayscale(100%); " width="85"  src="{{ url('/peddyhub/images/national-flag/flex-de.png') }}" onclick="change_language('de' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-md-4 col-lg-2 col-4">
+                                    <img class="btn" id="img_flag_ja" style="filter: grayscale(100%); " width="85"  src="{{ url('/peddyhub/images/national-flag/flex-ja.png') }}" onclick="change_language('ja' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-md-4 col-lg-2 col-4">
+                                    <img class="btn" id="img_flag_ko" style="filter: grayscale(100%); " width="85"  src="{{ url('/peddyhub/images/national-flag/flex-ko.png') }}" onclick="change_language('ko' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2" >
+                                    <img class="btn" id="img_flag_th" style="filter: grayscale(100%); " width="85" src="{{ url('/peddyhub/images/national-flag/flex-th.png') }}" onclick="change_language('th' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2">
+                                    <img class="btn" id="img_flag_lo" style="filter: grayscale(100%);"  width="85" src="{{ url('/peddyhub/images/national-flag/flex-la.png') }}" onclick="change_language('lo' , '{{ $data->id }}');">
+                                </div>
+                                <div class="col-4 col-md-4 col-lg-2 " >
+                                    <img class="btn" id="img_flag_my" style="filter: grayscale(100%); " width="85"  src="{{ url('/peddyhub/images/national-flag/flex-mm.png') }}" onclick="change_language('my' , '{{ $data->id }}');">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
