8a39d54806d3365c0ea25e0a60ccaefa6b6ed534
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-15 12:58:06 +0700

    Update index.blade.php

--- resources/views/lost_pet/index.blade.php
+++ resources/views/lost_pet/index.blade.php
@@ -35,6 +35,14 @@
                         <div class="col-12 col-md-9  ">
                             @include ('menubar.menu_btn')
                         </div>
+<<<<<<< Updated upstream
+=======
+                        <div class="col-3">
+                            <a href="{{ url('/lost_pet/create') }}" style="margin-top:8px" class="btn main float-right" title="contact">
+                                <span style="">ประกาศหา</span>  
+                            </a>
+                        </div>
+>>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -53,38 +61,32 @@
                                 <div class="card">
                                     <div  style="padding-top:10px;padding-left:20px;">
                                         <div class="row">
-                                            <!-- <span class="category">Pet Care</span> -->
                                             <div class="col-2" style="padding:0px;">
-                                            @if(!empty($item->user->photo))
-                                                <img style="border-radius: 50%;object-fit:cover; width:50px;height:50px;"  src="{{ url('storage')}}/{{ $item->user->photo }}" alt="image of client" title="client" class="img-fluid customer">
-                                            @else
-                                                <img style="border-radius: 50%;object-fit:cover; width:50px;height:50px;"  src="{{ url('peddyhub/images/home_5/icon1.png')}}" alt="image of client" title="client" class="img-fluid customer">
-                                            @endif
+                                                @if(!empty($item->user->photo))
+                                                    <img style="border-radius: 50%;object-fit:cover; width:50px;height:50px;"  src="{{ url('storage')}}/{{ $item->user->photo }}" alt="image of client" title="client" class="img-fluid customer">
+                                                @else
+                                                    <img style="border-radius: 50%;object-fit:cover; width:50px;height:50px;"  src="{{ url('peddyhub/images/home_5/icon1.png')}}" alt="image of client" title="client" class="img-fluid customer">
+                                                @endif
                                             </div>
                                             <div class="col-9" style="padding:0px">
-                                            @if(!empty($item->user->name))
-                                            <p class="notranslate" style="padding:0px;margin:0px;"> <b>{{ $item->user->name }}</b>  </p>
-                                                
-                                            @else
-                                                Guest
-                                            @endif
+                                                @if(!empty($item->user->name))
+                                                <p class="notranslate" style="padding:0px;margin:0px;"> <b>{{ $item->user->name }}</b>  </p>
+                                                    
+                                                @else
+                                                    Guest
+                                                @endif
                                                 <span style="font-size:20px"> <b></b> </span>
                                                 <p style="font-size:12px;margin-top:-8px;"> {{ $item->created_at->diffForHumans() }} </p>
                                             </div>
-                                            <div class="col-1" style="padding:0px;">
-                                                <a  type="button dropdown-toggle" id="dropdownMenu2" data-bs-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>
-                                            </div>
-                                            <div class="col-12" style="padding:0px 0px 0px 20px">
-                                                <a href="{{ url('/post/' . $item->id) }}" title="">
-                                                    <p class="head mt-1 mb-0">{{ $item->detail }} </p>
-                                                </a>
-                                            </div>
-                                            
                                         </div>
                                     </div>
                                     <div class="image">
                                         <img class="imgf" src="{{ url('storage/'.$item->photo )}}" width="400px" height="300px" alt="image of pet" title="pet" class="img-fluid customer">
+                                    </div>
+                                    <div class="col-12" style="padding:0px;">
+                                        <a href="{{ url('/lost_pet/' . $item->id) }}" class="col-6 main-shadow main-radius btn btn-info" style="margin:15px;float: right;">
+                                            รายละเอียด
+                                        </a>
                                     </div>
                                 </div>
                             </div>
