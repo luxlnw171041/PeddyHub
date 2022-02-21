55ea19a2398d5487368da2446bffed44762312bc
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-21 23:51:51 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -114,6 +114,14 @@ class LineApiController extends Controller
 
     public function set_richmanu_language($provider_id , $richMenuId , $user_language)
     {
+        //SAVE LOG
+            $data_CHECK = [
+                "title" => "CHECK",
+                // "content" => json_encode($data_users, JSON_UNESCAPED_UNICODE),
+                "content" => $provider_id,
+            ];
+            MyLog::create($data_CHECK);
+            
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_CLIENT_SECRET')]);
         $response = $bot->linkRichMenu($provider_id, $richMenuId);
