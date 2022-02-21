861076e50d27a9cfc2eec62561cac9f985e19337
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-21 22:21:03 +0700

    update

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -36,7 +36,7 @@ class LineApiController extends Controller
                 $this->save_group_line($event);
                 break;
             case "follow" :
-                // $this->user_follow_line($event);
+                $this->user_follow_line($event);
                 DB::table('users')
                     ->where([ 
                             ['provider_id', $event['source']['userId']],
@@ -48,6 +48,46 @@ class LineApiController extends Controller
         return $data ;
     }
 
+
+    public function user_follow_line($event)
+    {
+        $provider_id = $event['source']['userId'];
+
+        $opts = [
+            'http' =>[
+                'method'  => 'GET',
+                'header'  => 'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                //'timeout' => 60
+            ]
+        ];
+                            
+        $context  = stream_context_create($opts);
+
+        $url = "https://api.line.me/v2/bot/profile/".$provider_id;
+        $result = file_get_contents($url, false, $context);
+
+        $data_result = json_decode($result);
+
+        //SAVE LOG
+        $data = [
+            "title" => "ตรวจสอบภาษาเครื่องผู้ใช้",
+            "content" => $data_result->displayName . "-" . $data_result->language,
+        ];
+        MyLog::create($data);
+
+        $data_users = DB::table('users')
+            ->where('provider_id', $provider_id)
+            ->get();
+
+        // if (!empty($data_users[0])) {
+        //     // เช็คภาษาของ User
+        //     $this->check_language_user($data_users);
+        // }else {
+        //     // ตั้งค่าริชเมนูเริ่มต้น
+        //     $this->set_richmanu_start($provider_id , $data_result->language);
+        // }
+
+    }
     
 
 }
