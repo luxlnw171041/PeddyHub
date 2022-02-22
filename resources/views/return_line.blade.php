dcec91d38ff86d811388aaebeea2fbb063947a8a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-22 22:00:11 +0700

    Create return_line.blade.php

---
+++ resources/views/return_line.blade.php
@@ -0,0 +1,11 @@
+<a id="btn_addline" href="https://lin.ee/Bvi9Zr9">
+    <button type="button" class="btn btn-success"></button>
+</a>
+
+<script>
+    document.addEventListener('DOMContentLoaded', (event) => {
+        // console.log("START"); 
+        document.getElementById("btn_addline").click();
+
+    });
+</script>
