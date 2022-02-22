7e7ffbb76dd7a4db05aba4a453e2cdd71b496159
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-22 13:07:12 +0700

    Update API_language.php

--- app/Http/Controllers/API/API_language.php
+++ app/Http/Controllers/API/API_language.php
@@ -29,4 +29,66 @@ class API_language extends Controller
         return $language;
     }
 
+    // public function change_language_fromline($language , $user_id)
+    // {
+    //     DB::table('users')
+    //           ->where('id', $user_id)
+    //           ->update([
+    //             'language' => $language,
+    //     ]);
+
+    //     $data_users = DB::table('users')
+    //             ->where('id', $user_id)
+    //             ->where('status', "active")
+    //             ->get();
+
+    //     $lineAPI = new LineApiController();
+    //     $lineAPI->check_language_user($data_users);
+
+    //     foreach ($data_users as $key ) {
+    //       $provider_id = $key->provider_id ;
+    //     }
+
+    //     $data_Text_topic = [
+    //         "เปลี่ยนภาษาเรียบร้อยแล้ว",
+    //     ];
+
+    //     $data_topic = $this->language_for_user($data_Text_topic, $provider_id);
+
+    //     $template_path = storage_path('../public/json/change_language_success.json');   
+    //     $string_json = file_get_contents($template_path);
+    //     $string_json = str_replace("เปลี่ยนภาษาเรียบร้อยแล้ว",$data_topic[0],$string_json);
+
+    //     $messages = [ json_decode($string_json, true) ]; 
+
+    //     $body = [
+    //         "to" => $provider_id,
+    //         "messages" => $messages,
+    //     ];
+
+    //     $opts = [
+    //         'http' =>[
+    //             'method'  => 'POST',
+    //             'header'  => "Content-Type: application/json \r\n".
+    //                         'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+    //             'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+    //             //'timeout' => 60
+    //         ]
+    //     ];
+                            
+    //     $context  = stream_context_create($opts);
+    //     $url = "https://api.line.me/v2/bot/message/push";
+    //     $result = file_get_contents($url, false, $context);
+
+    //     //SAVE LOG
+    //     $data = [
+    //         "title" => "https://api.line.me/v2/bot/message/push",
+    //         "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
+    //     ];
+
+    //     MyLog::create($data);
+    //     // return $result;
+    //     return view('return_line');
+    // }
+
 }
