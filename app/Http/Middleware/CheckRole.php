04206c53ad082f46a091f84602e85df490d0ca4e
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-03-01 10:28:14 +0700

    role

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $allowed_list = array_slice(func_get_args(), 2);  
+        if (Auth::check()) {
+            $status = Auth::user()->profile->role;
+            //IF $status OF USER IS IN ALLOWED LIST
+            if (in_array($status, $allowed_list,True)){
+                return  $next($request);
+            }
+        } 
+        return redirect('/home');
+
+    }
+}
