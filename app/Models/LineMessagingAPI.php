6f73a406fd6c47e373d25faea6f7546b6052a4b5
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-03-16 16:02:58 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -115,27 +115,13 @@ class LineMessagingAPI extends Model
                 break;
             case 'blood_bank':
                     $provider_id = $event["source"]['userId'];
-
                     //จำนวนสัตว์ทั้งหมด
                     $count_pet = Blood_bank::where('user_id', $provider_id)
                         ->groupBy('pet_id')
                         ->get()->count();
-                    // จำนวนคร้งทั้งหมด
-                    $count_time = Blood_bank::where('user_id', $provider_id)
-                        ->selectRaw('count(pet_id) as count')
-                        ->count();
-                    //  ประมาณทั้งหมด
-                    $count_blood = Blood_bank::where('user_id', $provider_id)
-                        ->selectRaw('sum(total_blood) as count')
-                        ->get();
-                    foreach ($count_blood as $item) {
-                        $total_blood = $item->count ;
-                    }
-
+                
                     $template_path = storage_path('../public/json/flex_blood_bank.json');   
                     $string_json = file_get_contents($template_path);
-                    $string_json = str_replace("1500",$total_blood,$string_json);
-                    $string_json = str_replace("5",$count_time,$string_json);
                     $string_json = str_replace("3",$count_pet,$string_json);
                     $messages = [ json_decode($string_json, true) ]; 
                     break;
