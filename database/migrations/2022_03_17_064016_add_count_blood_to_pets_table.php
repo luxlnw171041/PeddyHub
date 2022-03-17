ab441f4a14592922096a2ec7d2a002ce6123e9a8
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-03-17 13:40:47 +0700

    Create 2022_03_17_064016_add_count_blood_to_pets_table.php

---
+++ database/migrations/2022_03_17_064016_add_count_blood_to_pets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCountBloodToPetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            $table->string('count_blood')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            //
+        });
+    }
+}
