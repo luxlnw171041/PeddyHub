bc448c90af6506e6069f10a8c9f8eb60df94d587
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-05-26 09:15:32 +0700

    Create Order.php

---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'orders';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'total', 'status', 'checking_at', 'tracking'];
+
+    public function order_products(){
+        return $this->hasMany('App\Models\OrderProduct', 'order_id'); 
+    } 
+}
