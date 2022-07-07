a9282218db585ba4e9e69f7e625162c4924a0db9
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-07-07 18:49:01 +0700

    Update form.blade.php

--- resources/views/pet/form.blade.php
+++ resources/views/pet/form.blade.php
@@ -822,21 +822,22 @@
                 <br>
                 <h5 style="font-family: 'Kanit', sans-serif;">สนับสนุนโดย</h5>
                 <div class="col-12 owl-carousel-two align-self-center" style="padding:0px;">
-                    <div class="owl-carousel">
+                <div class="owl-carousel">
                         @php
                         $partner = \App\Models\Partner::where(['show_homepage' => 'show'])->get()
                         @endphp
                         @foreach($partner as $item)
                             @if($item->id % 2 == 0)
-                                <div class="item" style="padding:0px;z-index:-1;">
+                            <div class="item" style="padding:0px;z-index:-1;">
                                 <div class="testimon">
                                     <a href="{{$item->link}}" target="bank">
                                         <img class="p-md-3 p-lg-3" style="width: 100%;object-fit: contain;max-height: 112px;" src="{{ url('storage/'.$item->logo )}}">
                                     </a>
                                 </div>
+                            </div>
                             @endif
                         @endforeach
-                        </div>
+                    </div>
                     <div class="owl-carousel">
                         @php
                         $partner = \App\Models\Partner::where(['show_homepage' => 'show'])->get()
