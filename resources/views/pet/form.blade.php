755a1397b82fd446bdc0048ac6ad23570fba94d1
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-07-07 18:31:07 +0700

    .

--- resources/views/pet/form.blade.php
+++ resources/views/pet/form.blade.php
@@ -769,7 +769,7 @@
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
                             <div class="d-flex justify-content-end">
-                                <button id="btn-modal" type="button" class="btn btn-11" data-toggle="modal" data-target="#modal_thx" onclick="submitform()" disabled>Update</button>
+                                <button id="btn-modal" type="button" class="btn btn-11" data-toggle="modal" data-target="#modal_thx" onclick="submitform()" >Update</button>
 
                                 <button id="btn-submit-form" type="submit" class="btn btn-11 form-control d-none" value="{{ $formMode === 'edit' ? 'Update' : 'ส่งข้อมูล' }}">Update</button>
                             </div>
@@ -807,6 +807,7 @@
         </div>
     </div>
 </div>
+
 <div class="modal fade " data-keyboard="false" id="modal_thx" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -827,14 +828,31 @@
                         $partner = \App\Models\Partner::where(['show_homepage' => 'show'])->get()
                         @endphp
                         @foreach($partner as $item)
-                        <div class="item" style="padding:0px;z-index:-1;">
-                            <div class="testimon">
-                                <a href="{{$item->link}}" target="bank">
-                                    <img class="p-md-3 p-lg-3" style="width: 100%;object-fit: contain;max-height: 112px;" src="{{ url('storage/'.$item->logo )}}">
-                                </a>
+                            @if($item->id % 2 == 0)
+                            <div class="item" style="padding:0px;z-index:-1;">
+                                <div class="testimon">
+                                    <a href="{{$item->link}}" target="bank">
+                                        <img class="p-md-3 p-lg-3" style="width: 100%;object-fit: contain;max-height: 112px;" src="{{ url('storage/'.$item->logo )}}">
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="owl-carousel">
+                        @php
+                        $partner = \App\Models\Partner::where(['show_homepage' => 'show'])->get()
+                        @endphp
+                        @foreach($partner as $item)
+                            @if($item->id % 2 != 0)
+                                <div class="item" style="padding:0px;z-index:-1;">
+                                    <div class="testimon">
+                                        <a href="{{$item->link}}" target="bank">
+                                            <img class="p-md-3 p-lg-3" style="width: 100%;object-fit: contain;max-height: 112px;" src="{{ url('storage/'.$item->logo )}}">
+                                        </a>
+                                    </div>
+                                </div>
+                            @endif
                         @endforeach
                     </div>
                 </div>
@@ -842,6 +860,7 @@
         </div>
     </div>
 </div>
+
 <input class="form-control" name="provider_id" type="hidden" id="provider_id" value="{{ isset($pet->provider_id) ? $pet->provider_id : Auth::user()->provider_id}}"  readonly>
 <input type="text" class="d-none" name="language_user" id="language_user" value="{{ Auth::user()->profile->language }}">
 <input type="text" class="d-none" name="certificate" id="input_certificate" value="{{ isset($pet->certificate) ? $pet->certificate : ''}}">
