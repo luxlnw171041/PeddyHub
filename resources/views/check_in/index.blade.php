a52cdeab38d7e7a63afdc769977dc75783b2fc52
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-07-08 20:27:33 +0700

    Update index.blade.php

--- resources/views/check_in/index.blade.php
+++ resources/views/check_in/index.blade.php
@@ -428,34 +428,87 @@
                 </div>
             </div>
         </div>
-    </div>
+        </div>
 </form>
 
 <!-- Modal send_finish -->
 <!-- Button trigger modal -->
-<button id="btn_modal_send_finish" class="btn btn-primary d-none" data-toggle="modal" data-target="#madal_send_finish">
-</button>
+<a id="btn_modal_send_finish" class=" d-" data-toggle="modal" data-target="#madal_send_finish">
+</a>
 <!-- Modal -->
 <div class="modal fade" id="madal_send_finish" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-body text-center">
-                <div class="wrapper">
-                    <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                        <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
-                        <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                    </svg>
-                    <h3 style="color: #7ac142;">แจ้งเตือนกลุ่มเสี่ยงเรียบร้อยแล้ว</h3>
-                </div>
-            </div>
-            <div class="modal-footer d-none">
-                <button id="close_madal_send_finish" type="button" class="btn btn-secondary" data-dismiss="modal">
-                    Close
-                </button>
-            </div>
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+    <div class="modal-body text-center">
+        <div class="wrapper">
+            <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+                <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+            </svg>
+            <h3 style="color: #7ac142;">แจ้งเตือนกลุ่มเสี่ยงเรียบร้อยแล้ว</h3>
         </div>
     </div>
-</div>
+    <div class="modal-footer d-none">
+        <button id="close_madal_send_finish" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    </div>
+        <!-- <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">Check_in</div>
+                        <div class="card-body">
+                            <a href="{{ url('/check_in/create') }}" class="btn btn-success btn-sm" title="Add New Check_in">
+                                <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                            </a>
+
+                            <form method="GET" action="{{ url('/check_in') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                                    <span class="input-group-append">
+                                        <button class="btn btn-secondary" type="submit">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                            </form>
+
+                            <br/>
+                            <br/>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th><th>User Id</th><th>Time In</th><th>Time Out</th><th>Check In At</th><th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($check_in as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->user_id }}</td><td>{{ $item->time_in }}</td><td>{{ $item->time_out }}</td><td>{{ $item->check_in_at }}</td>
+                                            <td>
+                                                <a href="{{ url('/check_in/' . $item->id) }}" title="View Check_in"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                                <a href="{{ url('/check_in/' . $item->id . '/edit') }}" title="Edit Check_in"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                                                <form method="POST" action="{{ url('/check_in' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Check_in" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                <div class="pagination-wrapper"> {!! $check_in->appends(['search' => Request::get('search')])->render() !!} </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
 
 <script>
 
@@ -652,7 +705,7 @@
                     class_div_btn_col_3.value = "col-3 text-center";
                     div_btn_col_3.setAttributeNode(class_div_btn_col_3);
 
-                    let btn_send = document.createElement("button");
+                    let btn_send = document.createElement("a");
                         btn_send.innerHTML = '<i class="fas fa-share-square"></i> ส่งข้อความเตือน !'
                     let class_btn_send = document.createAttribute("class");
                         class_btn_send.value = "btn btn-info text-white";
@@ -788,7 +841,7 @@
 
             document.querySelector('#btn_modal_send_finish').click();
             
-            var delayInMilliseconds = 4000; //
+            var delayInMilliseconds = 3500; //
             setTimeout(function() {
                 document.querySelector('#close_madal_send_finish').click();
             }, delayInMilliseconds);
