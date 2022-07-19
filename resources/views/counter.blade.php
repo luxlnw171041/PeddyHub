9d6140c6bdad1f74307e1c47c7010ba489bb63b3
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-07-19 17:24:31 +0700

    Update counter.blade.php

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -3,4 +3,8 @@
     <div class="นับ ">
         <a href="https://www.freecounterstat.com" title="page counter"><img src="https://counter9.stat.ovh/private/freecounterstat.php?c=xfj58s8yrgz8mpd9ccy3jy3uh1a29qql" border="0" title="page counter" alt="page counter"></a>
     </div>
+    <div class="d-none">
+        <script type="text/javascript" src="https://www.counters-free.net/count/b3bv"></script>
+    </div>
+    
 </center>
