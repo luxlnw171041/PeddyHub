2b05780b345a5abdb8d3d0f09c85c651fa020800
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-08-17 15:33:33 +0700

    Update create.blade.php

--- resources/views/pet/create.blade.php
+++ resources/views/pet/create.blade.php
@@ -15,7 +15,7 @@
                         <form method="POST" action="{{ url('/pet') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
                             {{ csrf_field() }}
 
-                            @include ('pet.form_create', ['formMode' => 'create'])
+                            @include ('pet.form', ['formMode' => 'create'])
 
                         </form>
             </div>
