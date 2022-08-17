804ef359e2ded3f884052536ff46bb6b401bd082
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-08-17 15:48:56 +0700

    Update form_create.blade.php

--- resources/views/pet/form_create.blade.php
+++ resources/views/pet/form_create.blade.php
@@ -168,8 +168,8 @@
 }
 </style>
 
-<!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script> -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
 
 <div class="main-wrapper pet check">
     <div class="pet service">
