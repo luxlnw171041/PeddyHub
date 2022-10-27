a9d681e648f67d3f1f8fc41b9cc1dcdd96a0de6a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-10-27 14:10:11 +0700

    Update test_for_devController.php

--- app/Http/Controllers/test_for_devController.php
+++ app/Http/Controllers/test_for_devController.php
@@ -39,8 +39,8 @@ class test_for_devController extends Controller
             'photo_link' => "https://www.peddyhub.com/storage/uploads/nkRauC1MfHHxpaU3OQ6IsC8GCfasu9cxBTOhF4Cr.jpg",
         ];
 
-        $url = 'http://localhost/PeddyHub/public/api/partner_lost_pet';
-        // $url = 'https://www.peddyhub.com/api/partner_lost_pet';
+        // $url = 'http://localhost/PeddyHub/public/api/partner_lost_pet';
+        $url = 'https://www.peddyhub.com/api/partner_lost_pet';
         $ch = curl_init($url);
 
         curl_setopt($ch, CURLOPT_POST, 1);
