595cd028446417b1e0edcbcc54a608d38e56497c
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-12-09 11:31:42 +0700

    Update LineMessagingAPI.php

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -708,7 +708,13 @@ class LineMessagingAPI extends Model
                             $i++;
                         }
                     }
-                    
+                    $string_json = str_replace("IMGPARTNER_1",$img_partner[0],$string_json);   
+                    $string_json = str_replace("IMGPARTNER_2",$img_partner[1],$string_json);   
+                    $string_json = str_replace("IMGPARTNER_3",$img_partner[2],$string_json);   
+                    $string_json = str_replace("IMGPARTNER_4",$img_partner[3],$string_json);   
+                    $string_json = str_replace("IMGPARTNER_5",$img_partner[4],$string_json);   
+                    $string_json = str_replace("IMGPARTNER_6",$img_partner[5],$string_json);   
+
                     $messages = [ json_decode($string_json, true) ];
 
 
