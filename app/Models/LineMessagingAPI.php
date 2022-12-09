935ed748af250453c41da65356284fecf5d5e5d8
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-12-09 09:32:36 +0700

    flex_lost_pet ครั้งที่2

--- app/Models/LineMessagingAPI.php
+++ app/Models/LineMessagingAPI.php
@@ -655,6 +655,7 @@ class LineMessagingAPI extends Model
                         }else{
                             $string_json = str_replace("lucky",$data_user->profile->name,$string_json);
                             $string_json = str_replace("0999999999",$phone,$string_json);
+                            $string_json = str_replace("PHONE_USER_TEL",$phone,$string_json);
                         }
                           
                     }
@@ -698,7 +699,7 @@ class LineMessagingAPI extends Model
                         
                         $string_json = str_replace("PET_NAME",$data_pet->name,$string_json);    
                         $string_json = str_replace("PET_AGE",$pet_age,$string_json);
-                        $string_json = str_replace("PETGENDER",$data_pet->gender,$string_json);
+                        $string_json = str_replace("PET_GENDER",$data_pet->gender,$string_json);
                     }
 
                     $messages = [ json_decode($string_json, true) ];
