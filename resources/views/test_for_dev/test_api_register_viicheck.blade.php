4516bbfabc1e26c21f8d38c0010b55127188f3f5
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-12-15 11:11:08 +0700

    Update test_api_register_viicheck.blade.php

--- resources/views/test_for_dev/test_api_register_viicheck.blade.php
+++ resources/views/test_for_dev/test_api_register_viicheck.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 	
+	<br><br><br><br><br><br><br><br>
 
 	<a href="https://www.viicheck.com/api/register_api?data_users={{ $data_users }}">
 	    <img class="wow fadeInUp" src="peddyhub/images/home_5/icon-line.png" alt="" width="35px">
