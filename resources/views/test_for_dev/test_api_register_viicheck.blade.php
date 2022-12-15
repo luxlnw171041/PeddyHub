1346a55f5c562c03d366bb0acec4a08c72af874f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-12-15 11:10:21 +0700

    Update test_api_register_viicheck.blade.php

--- resources/views/test_for_dev/test_api_register_viicheck.blade.php
+++ resources/views/test_for_dev/test_api_register_viicheck.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	
 
-	<a href="https://www.viicheck.com/api/register_api?requestArr={{ $requestArr }}">
+	<a href="https://www.viicheck.com/api/register_api?data_users={{ $data_users }}">
 	    <img class="wow fadeInUp" src="peddyhub/images/home_5/icon-line.png" alt="" width="35px">
 	</a>
 
