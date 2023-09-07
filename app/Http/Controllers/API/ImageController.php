bfe3f51b0d4eeb5f01ee8c74140801b02ac81536
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-09-07 08:47:51 +0700

    Update ImageController.php

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Intervention\Image\ImageManagerStatic as Image;
 use Auth;
